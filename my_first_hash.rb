978fa880a3a7eedad3b480a4b584d5c7da7e49dc
Sharon Watkins <swatkins876@gmail.com>
2019-09-13 15:12:57 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -40,5 +40,9 @@ shipping_manifest["gun powder"] = 4
 
 
   # return the shipping_manifest hash below
-
+shipping_manifest = {
+                  "whale bone corsets" => 5, 
+                  "porcelain vases" => 2, 
+                  "oil paintings" => 3
+                 }
 end
