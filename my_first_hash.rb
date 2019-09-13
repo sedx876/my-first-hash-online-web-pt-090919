059d4960efc98d097644c201de4a8e56b97ec520
Sharon Watkins <swatkins876@gmail.com>
2019-09-13 15:06:59 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-my_hash = {"person" => "Sharon", "age" => 43}
+your_hash = {"person" => "Sharon", "age" => 43}
   
 end
 
